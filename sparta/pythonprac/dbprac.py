ce4a44e4943ded82297111a854a205d9b429175a
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-10-24 17:00:35 +0900

    파이썬 몽고디비 연결

--- sparta/pythonprac/dbprac.py
+++ sparta/pythonprac/dbprac.py
@@ -1,3 +1,58 @@
 from pymongo import MongoClient
-client = MongoClient('여기에 URL 입력')
+client = MongoClient('mongodb+srv://test:sparta@cluster0.aik73au.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
+
+doc = {
+    'name':'bob',
+    'age':27
+}
+
+# 데이터 하나 insert
+# db.users.insert_one({'name':'bob', 'age':27})
+# db.users.insert_one({'name':'john', 'age':20})
+# db.users.insert_one({'name':'ann', 'age':20})
+
+# ---------------- 데이터 하나 삭제
+# db.users.delete_one({'name':'bob'})
+# db.users.insert_one({'name':'bobby', 'age':27})
+
+#-----------데이터 SELECT * FROM
+# all_users = list(db.users.find({},{'_id':False}))
+#
+# for user in all_users:
+#     print(user)
+#------------------하나의 데이터 찾기
+# user = db.users.find_one({'name':'bobby'},{'_id':False})
+# print(user)
+# print(user['age'])
+# ------------------ 데이터 수정
+# name이 bobby 인것을 찾아서, age를 19로 바꿔라~~~
+# db.users.update_one({'name':'bobby'},{'$set':{'age':19}})
+
+# user = db.users.find_one({'name':'bobby'},{'_id':False})
+# print(user)
+#------------------ 데이터 삭제
+# db.users.delete_one({'name':'bobby'})
+#
+# all_users = list(db.users.find({},{'_id':False}))
+#
+# for user in all_users:
+#     print(user)
+
+# 저장 - 예시
+doc = {'name':'bobby','age':21}
+db.users.insert_one(doc)
+
+# 한 개 찾기 - 예시
+user = db.users.find_one({'name':'bobby'})
+
+# 여러개 찾기 - 예시 ( _id 값은 제외하고 출력)
+all_users = list(db.users.find({},{'_id':False}))
+
+# 바꾸기 - 예시
+db.users.update_one({'name':'bobby'},{'$set':{'age':19}})
+
+# 지우기 - 예시
+db.users.delete_one({'name':'bobby'})
+
+
--- sparta/pythonprac/hello.py
+++ sparta/pythonprac/hello.py
@@ -83,14 +83,14 @@ print('---------------  크롤링 기본 세팅  ---------------')
 import requests
 from bs4 import BeautifulSoup
 
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.aik73au.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.naver?sel=pnt&date=20221024',headers=headers)
 
 soup = BeautifulSoup(data.text, 'html.parser')
-
-title = soup.select_one('#old_content > table > tbody > tr:nth-child(2) > td.title > div > a')
-rank =  soup.select_one('#old_content > table > tbody > tr:nth-child(2) > td:nth-child(1) > img')
-star =  soup.select_one('#old_content > table > tbody > tr:nth-child(2) > td.point')
 
 movies = soup.select('#old_content > table > tbody > tr')
 
