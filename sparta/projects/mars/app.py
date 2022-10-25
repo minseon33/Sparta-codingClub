4ae9c2d8d32508f041c7bb6ff679e702affae5d4
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-10-25 22:25:30 +0900

    화성땅 구매 연습

--- sparta/projects/mars/app.py
+++ sparta/projects/mars/app.py
@@ -1,19 +1,30 @@
 from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
 app = Flask(__name__)
-
+client = MongoClient('mongodb+srv://test:sparta@cluster0.aik73au.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
 @app.route('/')
 def home():
    return render_template('index.html')
 
 @app.route("/mars", methods=["POST"])
 def web_mars_post():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'POST 연결 완료!'})
+    name_receive = request.form['name_give']
+    address_receive = request.form['address_give']
+    size_receive = request.form['size_give']
+
+    doc = {
+        'name':name_receive,
+        'address':address_receive,
+        'size':size_receive
+    }
+    db.mars.insert_one(doc)
+    return jsonify({'msg': '주문완료'})
 
 @app.route("/mars", methods=["GET"])
 def web_mars_get():
-    return jsonify({'msg': 'GET 연결 완료!'})
+    order_list = list(db.mars.find({},{'_id':False}))
+    return jsonify({'orders':order_list})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
