7a8c1dd9a345df234e9aae4d178fc09df2c928f2
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-11-06 13:29:38 +0900

    스파르타 코딩캠프 자바 기초, 변수,상수

---
+++ spartaCamp/sparta_java/sparta_java/src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 5;
+        System.out.println(number);
+
+        String sparta = "hello Sparta";
+        System.out.println(sparta);
+
+        int minseonNumber = 33;
+        System.out.println("minseonNumber = "+ minseonNumber);
+
+        final int finalNumber = 1;
+
+    }
+}
