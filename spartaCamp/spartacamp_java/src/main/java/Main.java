f59bfa87180ef99c886baca60267767ea62a52cb
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-11-07 20:25:37 +0900

    자바기초 1-9 조건문까지

--- spartaCamp/spartacamp_java/src/main/java/Main.java
+++ spartaCamp/spartacamp_java/src/main/java/Main.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Main {
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+
+        if(score > 90 && score <= 100 ){
+            System.out.println("A등급 입니다.");
+        } else if (score > 80 && score <= 90) {
+            System.out.println("B등급 입니다.");
+        } else if (score > 70 && score <= 80) {
+            System.out.println("C등급 입니다.");
+        }else {
+            System.out.println("F등급 입니다.");
+        }
+
+        //문제 숫자를 입력받으면 입력받은 점수가 무슨 등급인지 출력
+        //91-100 까지는 A등급
+        //81-90 까지는 B
+        //71-80 까지는 C
+        // 그 외의 점수는 F등급...
+    }
 }
