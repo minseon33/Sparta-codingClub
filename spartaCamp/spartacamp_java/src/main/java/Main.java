aec251d3a8e8d7b24cfa8f8bd206775be3212a80
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-11-08 21:45:03 +0900

    no message

--- spartaCamp/spartacamp_java/src/main/java/Main.java
+++ spartaCamp/spartacamp_java/src/main/java/Main.java
@@ -4,8 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        for(int i =0; i<10; i++){
+        int sumOdd = 0;
+        int sumEven = 0;
+        for (int i = 0; i < 30; i++) {
+            int value = i + 1;
+            if (value % 2 == 0) {
+                sumEven += value;
+
+            } else {
+                sumOdd += value;
+
+            }
 
         }
+        System.out.println("짝수의 합 :" + sumOdd);
+        System.out.println("홀수의 합 :" + sumEven);
+
     }
 }
