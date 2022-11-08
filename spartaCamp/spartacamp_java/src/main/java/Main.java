bbf5b801c190fe5a858e1b5af08711cf659c4f35
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-11-08 11:12:36 +0900

    자바기초 반복문

--- spartaCamp/spartacamp_java/src/main/java/Main.java
+++ spartaCamp/spartacamp_java/src/main/java/Main.java
@@ -4,23 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int score = sc.nextInt();
+        for(int i =0; i<10; i++){
 
-        if(score > 90 && score <= 100 ){
-            System.out.println("A등급 입니다.");
-        } else if (score > 80 && score <= 90) {
-            System.out.println("B등급 입니다.");
-        } else if (score > 70 && score <= 80) {
-            System.out.println("C등급 입니다.");
-        }else {
-            System.out.println("F등급 입니다.");
         }
-
-        //문제 숫자를 입력받으면 입력받은 점수가 무슨 등급인지 출력
-        //91-100 까지는 A등급
-        //81-90 까지는 B
-        //71-80 까지는 C
-        // 그 외의 점수는 F등급...
     }
 }
