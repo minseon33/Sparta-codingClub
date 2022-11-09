4232511e69094284c2daae9699efdba82ad9def3
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-11-09 13:01:01 +0900

    파이썬 알고리즘 문제풀기 1번문제 정답풀이

---
+++ sparta_algorithm/week_1/01_01_find_max_num.py
@@ -0,0 +1,22 @@
+# 각 숫자마다 모든 다른 숫자와 비교해서 최대값인지 확인합니다.
+# 만약 다른 모든 값보다 크다면 반복문을 중단합니다.
+# (비교를 위해 조금 이상하게 구현했습니다. 앞으로 이 강의를 들으면 이렇게 작성하지 않으실 거예요!)
+
+
+input = [3, 5, 6, 1, 2, 4]
+
+
+def find_max_num(array):
+    for num in array:
+        for compare_num in array:
+            if num < compare_num:
+                break
+        else:
+            return num
+
+
+result = find_max_num(input)
+print(result)
+print("정답 = 6 / 현재 풀이 값 = ", find_max_num([3, 5, 6, 1, 2, 4]))
+print("정답 = 6 / 현재 풀이 값 = ", find_max_num([6, 6, 6]))
+print("정답 = 1888 / 현재 풀이 값 = ", find_max_num([6, 9, 2, 7, 1888]))
--- sparta_algorithm/week_1/01_02_find_max_num.py
+++ sparta_algorithm/week_1/01_02_find_max_num.py
@@ -1,16 +1,22 @@
+# 각 숫자마다 모든 다른 숫자와 비교해서 최대값인지 확인합니다.
+# 만약 다른 모든 값보다 크다면 반복문을 중단합니다.
+# (비교를 위해 조금 이상하게 구현했습니다. 앞으로 이 강의를 들으면 이렇게 작성하지 않으실 거예요!)
+
+
 input = [3, 5, 6, 1, 2, 4]
 
 
-def find_max_num(input):
-    max_num = 0
-    print(input)
-    for input_num in input:
-        print("현재의 input_num 값 :" + str(input_num))
-        if input_num > max_num:
-            max_num = input_num
-            print("현재의 max_num 값 : " + str(max_num))
+def find_max_num(array):
+    max_num = array[0]
+    for num in array:
+        if num > max_num:
+            max_num = num
     return max_num
 
+
+print("정답 = 6 / 현재 풀이 값 = ", find_max_num([3, 5, 6, 1, 2, 4]))
+print("정답 = 6 / 현재 풀이 값 = ", find_max_num([6, 6, 6]))
+print("정답 = 1888 / 현재 풀이 값 = ", find_max_num([6, 9, 2, 7, 1888]))
 
 result = find_max_num(input)
 print(result)
