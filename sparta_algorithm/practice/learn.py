2303a2a8db1025b152f39b9f755bb6333b662021
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-11-10 23:04:48 +0900

    파이썬 알고리즘 연습

--- sparta_algorithm/practice/learn.py
+++ sparta_algorithm/practice/learn.py
@@ -1,27 +1,16 @@
-num_list = [1, 2, 3, 6, 3, 2, 4, 5, 6, 2, 4]
-even_num = []
-odd_num = []
-sum_num = 0
-for num in num_list:
-    if num%2 == 0:
-        print("짝수입니다.")
-        even_num.append(num)
-    else :
-        print("홀수입니다.")
-        odd_num.append(num)
-
-print(len(even_num))
-print(len(odd_num))
-
-for num in num_list:
-    sum_num += num
-
-print(sum_num)
-
-max_num = 0
-
-for num in num_list:
-    if num > max_num:
-        max_num = num
-
-print(max_num)
+n, k = map(int,input().split())
+# input()으로 들어온 값을 split()으로 나눠서 int 로 변환 해준 뒤 맵으로 묶어준 것을 각각 n과 k에 넣는다.
+cnt = 0
+#갯수를 세야하니까 cnt 변수를 하나 지정해준다.
+for i in range(1,n+1):
+# 1부터 시작해서 n만큼의 길이만큼 반복문을 돌리는데
+    if n%i == 0:
+        #만약 n나누기 i 가 0과 같다면 cnt에 하나씩 더하며 넣어준다.
+        cnt +=1
+    if cnt == k:
+        #만약 cnt 값이 k와 같다면 i를 프린트 하고 함수를 빠져나온다.
+        print(i)
+        break
+else:
+    #만약 걸리는 k 값이 없다면 -1을 프린트해준다.
+    print(-1)
