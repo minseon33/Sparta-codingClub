9ca26691f922ea035e78f04307ad125994801245
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-11-11 09:55:26 +0900

    find 함수와 count 함수 연습

--- sparta_algorithm/practice/learn.py
+++ sparta_algorithm/practice/learn.py
@@ -6,3 +6,14 @@ print(msg.lower())
 #문자열을 대문자화
 print(msg.upper())
 print(msg)
+
+print("----------------------------------------")
+tmp=msg.upper()
+print(tmp)
+
+# 문자열을 찾아서 인덱스값을 반환하는 find함수
+print(tmp.find('T'))
+#find는 처음 발견한 T의 인덱스번호를 출력해준다.
+
+#문자열을 찾아 몇개인지 세어주는 count함수
+print(tmp.count('T'))
