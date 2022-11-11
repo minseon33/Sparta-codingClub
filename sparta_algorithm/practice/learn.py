42e12a459f2b259d087a29972985477fd981ef0b
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-11-11 20:17:29 +0900

    파이썬 알고리즘 연습, K번째 수 구하기

--- sparta_algorithm/practice/learn.py
+++ sparta_algorithm/practice/learn.py
@@ -1,65 +1,17 @@
-#문자열과 내장함수
-
-msg = "It is Time"
-# 문자열을 소문자화
-print(msg.lower())
-#문자열을 대문자화
-print(msg.upper())
-print(msg)
-
-print("----------------------------------------")
-tmp= msg.upper()
-print(tmp)
-
-# 문자열을 찾아서 인덱스값을 반환하는 find함수
-print(tmp.find('T'))
-#find는 처음 발견한 T의 인덱스번호를 출력해준다.
-
-#문자열을 찾아 몇개인지 세어주는 count함수
-print(tmp.count('T'))
-
-#문자열을 잘라주는 슬라이스
-print(msg)
-#msg 문자열의 처음부터 2번째 글자까지만 뽑아내겠다~~
-print(msg[:2])
-# >> 인덱스 2번 바로 앞에서 자르겠다.. 즉, 0,1 까지만 출력되므로 2개 글자만 출력된다.
-print(msg[3:5])
-# >> 인덱스번호 3번부터 인덱스번호 5번 전까지 뽑아내겠다.. 즉, 인덱스번호 3번,4번까지만 출력됨.
-print("----------------------------------------")
-#문자열의 길이를 구해주는 len() 함수
-print(len(msg))
-for i in range(len(msg)):
-    print(msg[i], end='/')
-print()
-for x in msg:
-    print(x, end='! ')
-print()
-# 문자열에서 대문자만 출력하기
-for s in msg:
-    if s.isupper():
-        print(s, end="/")
-print()
-#문자열에서 소문자만 출력하기
-for s in msg:
-    if s.islower():
-        print(s, end="/")
-print()
-#공백 제거하고 붙여서 출력하기
-for x in msg:
-    if x.isalpha():
-        print(x,end="")
-print()
-print("----------------------------------------")
-#아스키넘버 출력하는 ord()
-
-tmp = 'AZ'
-for x in tmp:
-    print(ord(x))
-print("****************************************")
-tmp = 'az'
-for z in tmp:
-    print(ord(z))
-print("****************************************")
-#아스키넘버에 대응되는 문자를 출력해주는 chr()
-tmp = 65
-print(chr(tmp))
+T = int(input())
+for t in range(T):
+    #range(t) 는 t 숫자만큼 반복할것이다.
+    n,s,e,k = map(int,input().split())
+    # input으로 들어온 값을 인트값으로 매핑(대응)해준다 n, s, e, k 변수에..
+    a = list(map(int,input().split()))
+    # input()으로 들어온 값을 인트값으로 매핑(대응)해줄건데 리스트로 만들어서 a 변수에 넣어준다.
+    print(a)
+    a = a[s-1:e]
+    #리스트 a의 s번째 부터 슬라이싱 해줄건데, 인덱스 번호로 뽑아줘야 하니까 s-1을 해주고,
+    # e번째 까지 뽑아주는건데, 인덱스 번호 앞까지 짤리는거니까, e는 굳이 -1을 안해줘도 된다.
+    print(a)
+    a.sort()
+    # a를 오름차순으로 정렬해주고, sort()의 기본값은 오름차순, 거꾸로 해주려면 reverse=True 해주어야 함.
+    print("#%d %d" %(t+1, a[k-1]))
+    # 출력할 때 포맷팅을 사용할건데, t가 인덱스 0번부터 도니까 #1로 출력되려면 t+1을 해줘야한다.
+    #a의 k번째 값을 찾아줘야 하는데, 인덱스가 0번부터 시작하니까 k-1을 해주어야 한다.
