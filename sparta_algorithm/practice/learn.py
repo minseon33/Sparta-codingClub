0f4991b052ed8d724ec240c51bd058fd7fb8bb43
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-11-14 15:31:07 +0900

    파이썬 알고리즘 연습 대표값 구하기 연습

--- sparta_algorithm/practice/learn.py
+++ sparta_algorithm/practice/learn.py
@@ -1,21 +1,5 @@
-import random as r
-a = []
-# print(a)
-b = list()
-# print(b)
+# 10
+# 45 73 66 87 92 67 75 79 78 80
+# output: 74 7
 
-a = [1,2,3,4,5]
-# print(a)
-# print(a[0])
 
-b=list(range(1,11))
-# print(b)
-
-c = a+b
-# print(c)
-
-print(a)
-a.append(6)
-print(a)
-a.insert(3,7)
-# 3번 인덱스에 7을 집어넣는다...
