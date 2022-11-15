f1037ded52db41d6873c555a265bc2ad580380b0
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-11-15 19:41:52 +0900

    파이썬 알고리즘 정다면체 정답코드

--- sparta_algorithm/practice/learn.py
+++ sparta_algorithm/practice/learn.py
@@ -1,28 +1,15 @@
-# 대표값 구하기
+# 입력 예제 : 4 6
 
-#입력예제 :
-# 10
-# 45 73 66 87 92 67 75 79 75 80
+n, m = map(int, input().split())
+cnt = [0]*(n+m+3)
+max = -2147000000
+for i in range(1,n+1): #n+1 까지 해야 n까지 돌게된다. 왜냐하면 range() 함수는 0부터 n-1까지 도니까.. range(4)하면 0,1,2,3 이렇게 돔.
+    for j in range(1,m+1): #주사위 눈이 1부터 시작하니까 1부터 돌아야 한다.
+        cnt[i+j] += 1
+for i in range(n+m+1):
+    if cnt[i]>max:
+        max = cnt[i]
 
-#출력예제 :
-# 74  7
-
-
-n = int(input())
-a = list(map(int,input().split()))
-ave = round(sum(a)/n)
-min = 2147000000
-
-for idx,x in enumerate(a):
-    #a의 인덱스 값을 idx에 넣어주고 x에 a 리스트의 값을 넣어서 대응해주는 함수 enumerate()
-    tmp = abs(x-ave)
-    if tmp < min :
-        min = tmp
-        score = x
-        res = idx+1 #학생번호 인데 인덱스가 0번부터 시작하니까 +1을 해주어야 한다.
-    #답이 2개인 경우 구현
-    elif tmp == min:
-        if x>score :
-            score = x
-            res = idx + 1
-print(ave, res)
+for i in range(n+m+1):
+    if cnt[i] == max:
+        print(i, end= ' ')
