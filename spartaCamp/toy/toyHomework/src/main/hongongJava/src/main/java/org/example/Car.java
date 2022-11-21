e3519e1057099bcbba32aa668a88f8da864cc303
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-11-21 17:09:30 +0900

    혼공자바 예제코드 연습 6-2챕터

--- spartaCamp/toy/toyHomework/src/main/hongongJava/src/main/java/org/example/Car.java
+++ spartaCamp/toy/toyHomework/src/main/hongongJava/src/main/java/org/example/Car.java
@@ -1,2 +1,4 @@
-package org.example;public class Car {
+package org.example;
+
+public class Car {
 }
--- spartaCamp/toy/toyHomework/src/main/hongongJava/src/main/java/org/example/Student.java
+++ spartaCamp/toy/toyHomework/src/main/hongongJava/src/main/java/org/example/Student.java
@@ -1,2 +1,4 @@
-package org.example;public class Student {
+package org.example;
+
+public class Student {
 }
